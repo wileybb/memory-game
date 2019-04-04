e83a66da15a5e32ed577ddb8da6b2c1959a779eb
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-04-04 12:05:46 -0700

    commiting

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -15,6 +15,8 @@ class Board extends React.Component {
         }
         this.handleClick = this.handleClick.bind(this)
         this.componentDidMount = this.componentDidMount.bind(this)
+        this.handleIncorrectGuess = this.handleIncorrectGuess.bind(this)
+        this.handleReset = this.handleReset.bind(this)
     }
 
     componentDidMount() {
@@ -24,6 +26,16 @@ class Board extends React.Component {
             lastClickedToken: null,
             score: 0
 
+        });
+    }
+
+    handleReset() {
+        this.setState({ 
+            pictures: this.shuffleData(this.state.pictures),
+            lastClickedType: null,
+            lastClickedToken: null,
+            score: 0,
+            headerText: ""
         });
     }
 
@@ -39,24 +51,37 @@ class Board extends React.Component {
         return data;
     };
 
+    handleIncorrectGuess() { 
+        this.setState(prevState=>{
+            const updatedPictures = prevState.pictures.map(item=>{
+                item.clicked = false
+                return item
+            })
+            return {
+                headerText: "Incorrect, try again",
+                lastClickedToken: null,
+                lastClickedType: null,
+                pictures: updatedPictures,
+                score: prevState.score
+            }
+        })
+    }
+         
+    
+
     handleClick(clickedItem) {
         console.log(clickedItem.typeId)
         console.log(clickedItem.tokenId)
         if(this.state.lastClickedToken === null){
             console.log("first guess")
-        } else {
-            console.log("second guess")
-        }
 
-        this.setState(prevState=>{
-            const updatedPictures = prevState.pictures.map(item => {
-                if(item.tokenId === clickedItem.tokenId) {
-                    item.clicked = !item.clicked
-                }
-                return item
-            })
-            if(prevState.lastClickedToken === null){
-                // it is the players first guess... 
+            this.setState(prevState=> {
+                const updatedPictures = prevState.pictures.map(item => {
+                    if(item.tokenId === clickedItem.tokenId) {
+                        item.clicked = !item.clicked
+                    }
+                    return item
+                })
                 return {
                     headerText: "Try to find a match...",
                     lastClickedType: clickedItem.typeId,
@@ -64,55 +89,104 @@ class Board extends React.Component {
                     pictures: updatedPictures,
                     score: prevState.score
                 }
-            }else {
-                //it is the players second guess...
+            })
 
-                let hiddenPictures = []
-                if(prevState.lastClickedType === clickedItem.typeId && prevState.lastClickedToken !== clickedItem.tokenId){
-                    //in the case the second guess was correct...
-                    hiddenPictures = prevState.pictures.map(item => {
-                        item.clicked = false
-                        if(item.typeId === clickedItem.typeId){
+            
+
+        } else {
+            //it is the user's seond guess
+            console.log("second guess")
+            if(this.state.lastClickedType === clickedItem.typeId && !(this.state.lastClickedToken === clickedItem.tokenId)){
+                //user's second guess was correct(match)
+                console.log("second guess was correct")
+                this.setState(prevState=>{
+                    const updatedPictures = prevState.pictures.map(item=>{
+                        if(item.typeId === prevState.lastClickedType) {
                             item.matched = true
                         }
+                        item.clicked = false
                         return item
                     })
+                    return{ 
+                            headerText: "You found a match!",
+                            lastClickedToken: null,
+                            lastClickedType: null,
+                            pictures: updatedPictures,
+                            score: prevState.score + 1
+                        }
+                })
+            } else {
+                console.log("second guess was incorrect")
+                this.handleIncorrectGuess()
+            }
 
-                    return { 
-                        headerText: "You found a match!",
-                        lastClickedToken: null,
-                        lastClickedType: null,
-                        pictures: hiddenPictures,
-                        score: prevState.score + 1
-                        }   
-                } else {
-                    //in the case the second guess was incorrect...
+        }
+
+        // this.setState(prevState=>{
+        //     const updatedPictures = prevState.pictures.map(item => {
+        //         if(item.tokenId === clickedItem.tokenId) {
+        //             item.clicked = !item.clicked
+        //         }
+        //         return item
+        //     })
+        //     if(prevState.lastClickedToken === null){
+        //         // it is the players first guess... 
+        //         return {
+        //             headerText: "Try to find a match...",
+        //             lastClickedType: clickedItem.typeId,
+        //             lastClickedToken: clickedItem.tokenId,
+        //             pictures: updatedPictures,
+        //             score: prevState.score
+        //         }
+        //     }else {
+        //         //it is the players second guess...
+
+        //         let hiddenPictures = []
+        //         if(prevState.lastClickedType === clickedItem.typeId && prevState.lastClickedToken !== clickedItem.tokenId){
+        //             //in the case the second guess was correct...
+        //             hiddenPictures = prevState.pictures.map(item => {
+        //                 item.clicked = false
+        //                 if(item.typeId === clickedItem.typeId){
+        //                     item.matched = true
+        //                 }
+        //                 return item
+        //             })
+
+        //             return { 
+        //                 headerText: "You found a match!",
+        //                 lastClickedToken: null,
+        //                 lastClickedType: null,
+        //                 pictures: hiddenPictures,
+        //                 score: prevState.score + 1
+        //                 }   
+        //         } else {
+        //             //in the case the second guess was incorrect...
              
                    
-                            hiddenPictures = prevState.pictures.map(item => {
-                                item.clicked = false
-                                return item
-                            })
+        //                     hiddenPictures = prevState.pictures.map(item => {
+        //                         item.clicked = false
+        //                         return item
+        //                     })
                    
-                            return {
-                                headerText: "Incorrect, try again",
-                                lastClickedToken: null,
-                                lastClickedType: null,
-                                pictures: hiddenPictures,
-                                score: prevState.score
-                            }
+        //                     return {
+        //                         headerText: "Incorrect, try again",
+        //                         lastClickedToken: null,
+        //                         lastClickedType: null,
+        //                         pictures: hiddenPictures,
+        //                         score: prevState.score
+        //                     }
                             
                       
                     
-                }
+        //         }
 
-            }
+        //     }
 
             
                 
             
 
-        })
+        // })
     }
 
     render() {
@@ -129,7 +203,7 @@ class Board extends React.Component {
             <div>
                 <h1>Score: {this.state.score}</h1>
                 <h1>{this.state.headerText}</h1>
-                <button onClick={this.componentDidMount}>Reset Game</button>
+                <button onClick={this.handleReset()}>Reset Game</button>
                 {pictureArray}
             </div>
         )
