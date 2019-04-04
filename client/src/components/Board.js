aef6417a8830b96ffbc27ef9aaf99aac26d49aa9
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-04-04 11:30:50 -0700

    board shuffle working

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -14,11 +14,40 @@ class Board extends React.Component {
 
         }
         this.handleClick = this.handleClick.bind(this)
+        this.componentDidMount = this.componentDidMount.bind(this)
     }
+
+    componentDidMount() {
+        this.setState({ 
+            pictures: this.shuffleData(this.state.pictures),
+            lastClickedType: null,
+            lastClickedToken: null,
+            score: 0
+
+        });
+    }
+
+    shuffleData = data => {
+        let i = data.length - 1;
+        while (i > 0) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = data[i];
+          data[i] = data[j];
+          data[j] = temp;
+          i--;
+        }
+        return data;
+    };
 
     handleClick(clickedItem) {
         console.log(clickedItem.typeId)
         console.log(clickedItem.tokenId)
+        if(this.state.lastClickedToken === null){
+            console.log("first guess")
+        } else {
+            console.log("second guess")
+        }
+
         this.setState(prevState=>{
             const updatedPictures = prevState.pictures.map(item => {
                 if(item.tokenId === clickedItem.tokenId) {
@@ -39,7 +68,7 @@ class Board extends React.Component {
                 //it is the players second guess...
 
                 let hiddenPictures = []
-                if(prevState.lastClickedType === clickedItem.typeId && prevState.lastClickedToken != clickedItem.tokenId){
+                if(prevState.lastClickedType === clickedItem.typeId && prevState.lastClickedToken !== clickedItem.tokenId){
                     //in the case the second guess was correct...
                     hiddenPictures = prevState.pictures.map(item => {
                         item.clicked = false
@@ -58,17 +87,23 @@ class Board extends React.Component {
                         }   
                 } else {
                     //in the case the second guess was incorrect...
-                    hiddenPictures = prevState.pictures.map(item => {
-                        item.clicked = false
-                        return item
-                    })
-                    return {
-                        headerText: "Incorrect, try again",
-                        lastClickedToken: null,
-                        lastClickedType: null,
-                        pictures: hiddenPictures,
-                        score: prevState.score
-                    }
+             
+                   
+                            hiddenPictures = prevState.pictures.map(item => {
+                                item.clicked = false
+                                return item
+                            })
+                   
+                            return {
+                                headerText: "Incorrect, try again",
+                                lastClickedToken: null,
+                                lastClickedType: null,
+                                pictures: hiddenPictures,
+                                score: prevState.score
+                            }
+                            
+                      
+                    
                 }
 
             }
@@ -94,6 +129,7 @@ class Board extends React.Component {
             <div>
                 <h1>Score: {this.state.score}</h1>
                 <h1>{this.state.headerText}</h1>
+                <button onClick={this.componentDidMount}>Reset Game</button>
                 {pictureArray}
             </div>
         )
