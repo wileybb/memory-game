eb863c5dfd92ab49f60fb7a0775207bec3927e22
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-04-09 11:50:12 -0700

    Updates

---
+++ precache-manifest.2e9426bc4f49577d97837b04d93c13ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "1b949f4e22829a88e660",
+    "url": "/memory-game/static/js/main.d8d0b2de.chunk.js"
+  },
+  {
+    "revision": "28fb50aa736e5109e2fc",
+    "url": "/memory-game/static/js/2.234adc25.chunk.js"
+  },
+  {
+    "revision": "1b949f4e22829a88e660",
+    "url": "/memory-game/static/css/main.4cbfff55.chunk.css"
+  },
+  {
+    "revision": "8addecc57945d71d8282283a1ff36f60",
+    "url": "/memory-game/index.html"
+  }
+];
